3857fb400fad31885889c46b591157d1e8abcbd5
Sebastian Uribe <suribe@gmail.com>
2020-06-01 14:30:07 +0200

    Move dictionary to new file

---
+++ dictionary.js
@@ -0,0 +1,48 @@
+
+const Dictionary = [
+  new Word("Bus", "Busse", [Genders.Maskulin], 1),
+  new Word("Zimmer", "Zimmer", Genders.Neutrum, 1),
+  new Word("Tür", "Türen", Genders.Feminin, 2),
+  new Word("Held", "Helden", Genders.Maskulin, 3),
+  new Word("Kind", "Kinder", Genders.Neutrum, 4),
+  new Word("Hand", "Hände", Genders.Feminin, 5),
+  new Word("Opa", "Opas", Genders.Maskulin, 6),
+  new Word("Mama", "Mamas", Genders.Feminin, 6),
+  new Word("Komma", "Kommas", Genders.Neutrum, 6),
+
+  new Word("Bahn", "Bahnen", Genders.Feminin, 2),
+  new Word("Amt", "Ämter", Genders.Neutrum, 4),
+  new Word("Wort", "Wörter", Genders.Neutrum, 4),
+  new Word("Messe", "Messen", Genders.Feminin, 2),
+  new Word("Spiel", "Spiele", Genders.Neutrum, 1),
+    
+  new Word("Anlage", "Anlagen", Genders.Feminin, 2),
+  new Word("Welle", "Wellen", Genders.Feminin, 2),
+  new Word("Messer", "Messer", Genders.Neutrum, 1),
+  new Word("Mantel", "Mäntel", Genders.Maskulin, 1),
+  new Word("Schuh", "Schuhe", Genders.Maskulin, 1),
+  new Word("Ampel", "Ampeln", Genders.Feminin, 2),
+  new Word("Wurst", "Würste", Genders.Feminin, 5),
+  new Word("Hase", "Hasen", Genders.Maskulin, 3),
+  new Word("Lied", "Lieder", Genders.Neutrum, 4),
+  new Word("Wald", "Wälder", Genders.Maskulin, 1), // confirm type
+  new Word("Büro", "Büros", Genders.Neutrum, 6),
+  new Word("Kolibri", "Kolibris", Genders.Maskulin, 6),
+  new Word("Datum", "Daten", Genders.Neutrum, 1, [{ kasus: Cases.Dativ, form: "Daten"}]), // confirm type
+  new Word("Vater", "Väter", Genders.Maskulin, 1),
+  new Word("Mutter", "Mütter", Genders.Feminin, 2),
+  new Word("Pferd", "Pferde", Genders.Neutrum, 1),
+  new Word("Tisch", "Tische", Genders.Maskulin, 1),
+  new Word("Feder", "Federn", Genders.Feminin, 2),
+
+  new Word("Koch", "Köche", Genders.Maskulin, 1),
+  new Word("Hund", "Hunde", Genders.Maskulin, 1),
+  new Word("Gans", "Gänse", Genders.Feminin, 5),
+  new Word("Haus", "Häuser", Genders.Neutrum, 4),
+  new Word("Mund", "Münder", Genders.Maskulin, 1),
+  new Word("Name", "Namen", Genders.Maskulin, 3),
+  new Word("Lehre", "Lehren", Genders.Feminin, 2),
+  new Word("Kino", "Kinos", Genders.Neutrum, 6),
+  new Word("Radio", "Radios", Genders.Neutrum, 6),
+  new Word("Onkel", "Onkel", Genders.Maskulin, 1),
+];
